37b6b746fced037f6afd1a321b1ee4e34a038651
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-10-15 19:40:43 -0300

    initial commit

---
+++ backend/app/Models/Farm.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Farm extends Model
+{
+    use HasFactory;
+
+     protected $fillable = [
+        'owners',
+        'total_of_animals'
+    ];
+
+    public function rules()
+    {
+        return [
+            'owners' => 'required|array',
+            'total_of_animals' => 'required|numeric',
+        ];
+    }
+
+}
