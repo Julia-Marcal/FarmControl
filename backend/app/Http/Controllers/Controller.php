3afd36f9dee8f45c556147c617d99a6599a36f2c
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-10-21 18:15:37 -0300

    creating base controller

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -5,8 +5,61 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+     public function index()
+    {
+        $data = $this->model->all();
+        return response()->json($data);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, $this->model->rules());
+
+        $dataForm = $request->all();
+
+        $data = $this->model->create($dataForm);
+
+        return response()->json($data, 201);
+    }
+
+    public function show($id)
+    {
+        if (!$data = $this->model->find($id)) {
+            return response()->json(['error' => 'Nada foi encontrado!'], 404);
+        } else {
+            return response()->json($data);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        if (!$data = $this->model->find($id))
+            return response()->json(['error' => 'Nada foi encontrado!'], 404);
+
+        $this->validate($request, $this->model->rules());
+
+        $dataForm = $request->all();
+
+        $data->update($dataForm);
+
+        return response()->json($data);
+    }
+
+
+    public function destroy($id)
+    {
+        if ($data = $this->model->find($id)) {
+
+            $data->delete();
+            return response()->json(['success' => 'Deletado com sucesso!']);
+        } else {
+            return response()->json(['error' => 'Nada foi encontrado!'], 404);
+        }
+    }
 }
