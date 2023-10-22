5f439de7b21e029adcea742c6ce55c8841af4cd6
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-10-22 16:17:28 -0300

    creating controller for users

---
+++ backend/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    protected $model;
+
+    public function __construct(User $Users)
+    {
+        $this->model = $Users;
+    }
+
+    public function index()
+    {
+        $data = $this->model->all();
+        return response()->json($data);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, $this->model->rules());
+
+        $dataForm = $request->all();
+
+        $data = $this->model->create($dataForm);
+
+        return response()->json($data, 201);
+    }
+}
+
