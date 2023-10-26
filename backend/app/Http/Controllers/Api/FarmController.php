e7bb73f1ae58752a29a523b7b2b22e58b2b70fc3
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-10-26 20:13:09 -0300

    add: adding sql pure query instead of php

--- backend/app/Http/Controllers/Api/FarmController.php
+++ backend/app/Http/Controllers/Api/FarmController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Farm;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class FarmController extends Controller
 {
@@ -17,7 +18,8 @@ class FarmController extends Controller
 
     public function index()
     {
-        $data = $this->model->all();
+        $data = DB::select('SELECT * FROM farms');
+
         return response()->json($data);
     }
 
