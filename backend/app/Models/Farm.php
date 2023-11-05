64dbe7fc2d228129a76428eeb6c8dcfb96e92001
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-11-05 17:49:19 -0300

    adapting farm model

--- backend/app/Models/Farm.php
+++ backend/app/Models/Farm.php
@@ -10,7 +10,7 @@ class Farm extends Model
     use HasFactory;
 
      protected $fillable = [
-        'owners',
+        'owner_id',
         'total_of_animals',
         'baby_animals',
         'adult_animals',
@@ -19,10 +19,22 @@ class Farm extends Model
         'total_expenses'
     ];
 
+    protected static function booted()
+    {
+        static::creating(function ($farm) {
+            $farm->total_of_animals = 0;
+            $farm->baby_animals = 0;
+            $farm->adult_animals = 0;
+            $farm->expenses_adults = 0;
+            $farm->expenses_babies = 0;
+            $farm->total_expenses = 0;
+        });
+    }
+
     public function rules()
     {
         return [
-            'owners' => 'required',
+            'owner_id' => 'required',
             'total_of_animals' => 'required|numeric',
             'child_animals' => 'numeric',
             'adult_animals' => 'numeric',
